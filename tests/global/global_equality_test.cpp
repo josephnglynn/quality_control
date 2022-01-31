a4c24794a7e90f5f0e3edf323fe2a66059e2f31d
Joseph <56293291+josephnglynn@users.noreply.github.com>
2022-01-31 22:04:52 +0000

    Updated tests to test functions in both global and non-global locations

---
+++ tests/global/global_equality_test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by joseph on 31/01/2022.
+//
+
+#define TEST_FUNCTION CHECK_EQUAL
+#include "../common/equality_test.hpp"
+
+int main()
+{
+	return run_tests();
+}
--- tests/utils.hpp
+++ tests/utils.hpp
@@ -5,19 +5,27 @@
 #ifndef TESTS_UTILS_H
 #define TESTS_UTILS_H
 
+#define LOGGER_ALWAYS_OUTPUT
 #include <functional>
+#include <logger/logger.hpp>
 #include <string>
 
 namespace utils
 {
 
-	template<typename A, typename B>
-	constexpr inline void test_numbers(std::function<void(A, B)> func)
+	inline void error()
+	{
+		logger::error("Error in test case");
+		std::exit(-1);
+	}
+
+	template<typename A, typename B, bool negative = true>
+	constexpr inline void test_numbers(std::function<bool(A, B)> func)
 	{
 		A a;
 		B b;
 
-		if (std::is_signed_v<A>)
+		if constexpr (std::is_signed_v<A> && negative)
 		{
 			a = -8;
 		}
@@ -26,33 +34,37 @@ namespace utils
 			a = 0;
 		}
 
-		if (std::is_signed_v<B>) {
+		if constexpr (std::is_signed_v<B> && negative)
+		{
 			b = -8;
-		} else {
+		}
+		else
+		{
 			b = 0;
 		}
 
-
-		func(a, b);
+		if (!func(a, b)) error();
 
 		a = b = 2;
-		func(a, b);
+		if (!func(a, b)) error();
 
 		a = b = 1;
-		func(a, b);
+		if (!func(a, b)) error();
 
 		a = b = 4;
-		func(a, b);
+		if (!func(a, b)) error();
 	}
 
 	template<typename T>
-	constexpr inline void test_numbers(std::function<void(T, T)> func)
+	constexpr inline void test_numbers(std::function<bool(T, T)> func)
 	{
 		return test_numbers<T, T>(func);
 	}
 
 	template<typename A, typename B>
-	constexpr inline void test_string(std::function<void(const std::basic_string<A>& a, const std::basic_string<B>& b)> func)
+	constexpr inline void test_string(std::function<void(const std::basic_string<A>& a,
+														 const std::basic_string<B>& b)>
+										  func)
 	{
 		std::basic_string<A> a = (const A*)"Test_String";
 		std::basic_string<B> b = (const B*)"Test_String";
